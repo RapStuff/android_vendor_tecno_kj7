a8332209ce1d5f9f734d3419423c6ecc00ddc6cd
Rapli <raffikun1203@gmail.com>
2026-08-29 01:24:18 +0700

    vendor: Fix radio images SHA1 mismatch

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),kj7)
 
-$(call add-radio-file-sha1-checked,radio/gz.img,cd7cb8db66a47e88e8ffb140e374ff65bc6d9273)
-$(call add-radio-file-sha1-checked,radio/lk.img,48b6d11e423c525cb704a528453377ce0f674703)
-$(call add-radio-file-sha1-checked,radio/logo.img,e1afce034c02f743ac84f1d33799b58a3bb5a5b4)
-$(call add-radio-file-sha1-checked,radio/md1img.img,2213d18d3c28152d38596cc950eb552f7250df79)
-$(call add-radio-file-sha1-checked,radio/scp.img,344b8b1a57af26f48562e51c2e0053ee00719f35)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,b6f44cef47dccd09eb0f0c88fefe50d096c47ccd)
-$(call add-radio-file-sha1-checked,radio/sspm.img,af3023ac3276106da4e2d917226c6928b387714a)
-$(call add-radio-file-sha1-checked,radio/tee.img,e57595e5f155a7e6a730bc532c10513151f1bbae)
-$(call add-radio-file-sha1-checked,radio/tkv.img,28059d1f168a4d63afe1ae41c17b35a303f46e97)
+$(call add-radio-file-sha1-checked,radio/gz.img,16f92640bd397e301d9e672d02dc4b92b53b5663)
+$(call add-radio-file-sha1-checked,radio/lk.img,2ce3cf9203d6998c4462bf08cdd486dbe43f6fe7)
+$(call add-radio-file-sha1-checked,radio/logo.img,985d06aa11b471530c4b7a4c0ec945c96c802a83)
+$(call add-radio-file-sha1-checked,radio/md1img.img,8bb7d1ea746c452a63b929bdea94e3123203dd9a)
+$(call add-radio-file-sha1-checked,radio/scp.img,40c8567ab6495f107123cad020eec95c80ae2b16)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,4696ae72cf46e7aca9628894e308ff7dc0563578)
+$(call add-radio-file-sha1-checked,radio/sspm.img,a9b05119ee6918a7b57e61925a70fde951806717)
+$(call add-radio-file-sha1-checked,radio/tee.img,7ea04db98700fc3e91f3a5ad57ce7f6ea04393f4)
+$(call add-radio-file-sha1-checked,radio/tkv.img,32866c9a3379dc8e851304d79097893882a4786a)
 
 endif
