313130ea8bcda36fa9a4beed7e3d4e3779ace8f0
Rapli <raffikun1203@gmail.com>
2026-08-28 09:37:33 +0700

    Initial bring-up vendor tecno KJ7

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),KJ7)
+
+$(call add-radio-file-sha1-checked,radio/gz.img,cd7cb8db66a47e88e8ffb140e374ff65bc6d9273)
+$(call add-radio-file-sha1-checked,radio/lk.img,48b6d11e423c525cb704a528453377ce0f674703)
+$(call add-radio-file-sha1-checked,radio/logo.img,e1afce034c02f743ac84f1d33799b58a3bb5a5b4)
+$(call add-radio-file-sha1-checked,radio/md1img.img,2213d18d3c28152d38596cc950eb552f7250df79)
+$(call add-radio-file-sha1-checked,radio/scp.img,344b8b1a57af26f48562e51c2e0053ee00719f35)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b6f44cef47dccd09eb0f0c88fefe50d096c47ccd)
+$(call add-radio-file-sha1-checked,radio/sspm.img,af3023ac3276106da4e2d917226c6928b387714a)
+$(call add-radio-file-sha1-checked,radio/tee.img,e57595e5f155a7e6a730bc532c10513151f1bbae)
+$(call add-radio-file-sha1-checked,radio/tkv.img,28059d1f168a4d63afe1ae41c17b35a303f46e97)
+
+endif
